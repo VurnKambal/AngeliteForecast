d111dd8c802593db8c8ea0fe1b40106bcc7a0ac7
Verneil Kent Batiller <vurnkambal@gmail.com>
2024-09-30 22:55:44 +0800

    adjusted link routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,11 @@ const Welcome = lazy(() => import('../pages/protected/Welcome'))
 const Page404 = lazy(() => import('../pages/protected/404'))
 const Blank = lazy(() => import('../pages/protected/Blank'))
 const Charts = lazy(() => import('../pages/protected/Charts'))
-const Leads = lazy(() => import('../pages/protected/Leads'))
+const Factors = lazy(() => import('../pages/protected/Leads'))
 const Integration = lazy(() => import('../pages/protected/Integration'))
 const Calendar = lazy(() => import('../pages/protected/Calendar'))
 const Team = lazy(() => import('../pages/protected/Team'))
-const Transactions = lazy(() => import('../pages/protected/Transactions'))
+const Enrollments = lazy(() => import('../pages/protected/Transactions'))
 const Bills = lazy(() => import('../pages/protected/Bills'))
 const ProfileSettings = lazy(() => import('../pages/protected/ProfileSettings'))
 const GettingStarted = lazy(() => import('../pages/GettingStarted'))
@@ -29,8 +29,8 @@ const routes = [
     component: Welcome, // view rendered
   },
   {
-    path: '/leads',
-    component: Leads,
+    path: '/factors',
+    component: Factors,
   },
   {
     path: '/settings-team',
@@ -41,8 +41,8 @@ const routes = [
     component: Calendar,
   },
   {
-    path: '/transactions',
-    component: Transactions,
+    path: '/enrollments',
+    component: Enrollments,
   },
   {
     path: '/settings-profile',
@@ -65,13 +65,13 @@ const routes = [
     component: DocComponents,
   },
   {
-    path: '/integration',
+    path: '/forecast',
     component: Integration,
   },
-  {
-    path: '/charts',
-    component: Charts,
-  },
+  // {
+  //   path: '/charts',
+  //   component: Charts,
+  // },
   {
     path: '/404',
     component: Page404,
