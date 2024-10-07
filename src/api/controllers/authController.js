090a6f8e8421f87b3edcde35524ad2ab7ebc8ac0
Verneil Kent Batiller <vurnkambal@gmail.com>
2024-10-07 14:34:09 +0800

    unused

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,28 +1,28 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const User = require('../models/userModel');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
 
-exports.register = async (req, res) => {
-    const { username, email, password } = req.body;
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ username, email, password_hash: hashedPassword });
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
+// exports.register = async (req, res) => {
+//     const { username, email, password } = req.body;
+//     try {
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         const user = await User.create({ username, email, password_hash: hashedPassword });
+//         res.status(201).json({ message: 'User registered successfully' });
+//     } catch (error) {
+//         res.status(400).json({ error: error.message });
+//     }
+// };
 
-exports.login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email } });
-        if (!user || !await bcrypt.compare(password, user.password_hash)) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-        const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
-        res.json({ token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
+// exports.login = async (req, res) => {
+//     const { email, password } = req.body;
+//     try {
+//         const user = await User.findOne({ where: { email } });
+//         if (!user || !await bcrypt.compare(password, user.password_hash)) {
+//             return res.status(401).json({ message: 'Invalid credentials' });
+//         }
+//         const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
+//         res.json({ token });
+//     } catch (error) {
+//         res.status(400).json({ error: error.message });
+//     }
+// };
